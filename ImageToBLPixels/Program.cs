296b239f09f3ad14408a3fe90d45a9a98a277152
Mqc <87596784+MqcCheeze@users.noreply.github.com>
2024-03-09 23:09:51 +0000

    Update Program.cs

--- ImageToBLPixels/Program.cs
+++ ImageToBLPixels/Program.cs
@@ -12,42 +12,48 @@ namespace ImageToBLPixels {
                 string filePath = args[0];
                 Console.WriteLine("File found at - " + filePath);
 
+                Image image;
+
                 try {
 
-                    using (Image image = Image.FromFile(filePath)) {
-
-                        int blackPixels = 0;
-                        int whitePixels = 0;
-
-                        for (int y = 0; y < image.Height; y++) {
-                            for (int x = 0; x < image.Width; x++) {
-                                Color pixelColor = ((Bitmap)image).GetPixel(x, y);
-
-                                int threshold = 128;
-                                Color newColor = (pixelColor.R + pixelColor.G + pixelColor.B) / 3 < threshold ? Color.Black : Color.White;
-
-                                if (pixelColor.ToArgb() == Color.Black.ToArgb()) {
-                                    blackPixels++;
-                                } else {
-                                    whitePixels++;
-                                }
-                            }
-                        }
-
-                        Console.WriteLine("");
-                        Console.WriteLine("Number of black pixels: " + blackPixels);
-                        Console.WriteLine("Number of white pixels: " + whitePixels);
-
-                        float blackToWhiteRatio = (float)blackPixels / whitePixels;
-                        blackToWhiteRatio = (float)Math.Round(blackToWhiteRatio, 5);
-
-                        Console.WriteLine("Roughly " + blackToWhiteRatio + " for every white pixel");
-
-                    }
-                    
+                    image = Image.FromFile(filePath);
                 } catch (ArgumentException) {
 
                     Console.WriteLine("File specified is not an image file...");
+                    return;
+                }
+
+                try {
+
+                    int blackPixels = 0;
+                    int whitePixels = 0;
+
+                    for (int y = 0; y < image.Height; y++) {
+                        for (int x = 0; x < image.Width; x++) {
+                            Color pixelColor = ((Bitmap)image).GetPixel(x, y);
+
+                            int threshold = 128;
+                            Color newColor = (pixelColor.R + pixelColor.G + pixelColor.B) / 3 < threshold ? Color.Black : Color.White;
+
+                            if (pixelColor.ToArgb() == Color.Black.ToArgb()) {
+                                blackPixels++;
+                            } else {
+                                whitePixels++;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("");
+                    Console.WriteLine("Number of black pixels: " + blackPixels);
+                    Console.WriteLine("Number of white pixels: " + whitePixels);
+
+                    float blackToWhiteRatio = (float)blackPixels / whitePixels;
+                    blackToWhiteRatio = (float)Math.Round(blackToWhiteRatio, 5);
+
+                    Console.WriteLine("Roughly " + blackToWhiteRatio + " for every white pixel");
+                } catch {
+
+                    Console.WriteLine("Error while converting/calculating image...");
                     return;
                 }
             } else {
