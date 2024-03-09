45c89db9c1e85d3f396c371dab29db6e71ea0460
Mqc <87596784+MqcCheeze@users.noreply.github.com>
2024-03-09 23:05:31 +0000

    Added sln

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace ImageToBLPixels {
+    public static class Program {
+
+        public static void Main(String[] args) {
+
+            if (args.Length != 0) {
+
+                string filePath = args[0];
+                Console.WriteLine("File found at - " + filePath);
+
+                try {
+
+                    using (Image image = Image.FromFile(filePath)) {
+
+                        int blackPixels = 0;
+                        int whitePixels = 0;
+
+                        for (int y = 0; y < image.Height; y++) {
+                            for (int x = 0; x < image.Width; x++) {
+                                Color pixelColor = ((Bitmap)image).GetPixel(x, y);
+
+                                int threshold = 128;
+                                Color newColor = (pixelColor.R + pixelColor.G + pixelColor.B) / 3 < threshold ? Color.Black : Color.White;
+
+                                if (pixelColor.ToArgb() == Color.Black.ToArgb()) {
+                                    blackPixels++;
+                                } else {
+                                    whitePixels++;
+                                }
+                            }
+                        }
+
+                        Console.WriteLine("");
+                        Console.WriteLine("Number of black pixels: " + blackPixels);
+                        Console.WriteLine("Number of white pixels: " + whitePixels);
+
+                        float blackToWhiteRatio = (float)blackPixels / whitePixels;
+                        blackToWhiteRatio = (float)Math.Round(blackToWhiteRatio, 5);
+
+                        Console.WriteLine("Roughly " + blackToWhiteRatio + " for every white pixel");
+
+                    }
+                    
+                } catch (ArgumentException) {
+
+                    Console.WriteLine("File specified is not an image file...");
+                    return;
+                }
+            } else {
+
+                Console.WriteLine("No file detected...");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
